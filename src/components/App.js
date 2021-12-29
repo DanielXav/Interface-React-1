34019c42c79ca25dfed187e62ac4a0d1f45e8cf2
Daniel Xavier <dxavier194@gmail.com>
2021-12-29 11:03:13 -0300

    Adicionado funcionalidade de mostrar e esconder imagem

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import Image from "./Image";
 
 const App = () => (
     <div>
         <Header />
         <Main />
+        <Image />
         <Footer />
     </div>
 );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,9 @@
 import { Component } from 'react';
-import imagem from '../images/imagem.jpg'
 
 export default class Footer extends Component {
     render() {
       return (
         <footer>
-            <abbr className='centro' title="Pica Pau"><img src={ imagem }/></abbr><br/>
-            <span className='centro'>Pica Pau</span>
             <p className='centro' ><abbr title="Centro de Ciência e Tecnologia">CCT</abbr></p>
         </footer>
       );
